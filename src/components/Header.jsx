53693d5456ba16ca933d48ff0dae96861506f723
Kenneth Sonoda <114664532+Kenny-still12@users.noreply.github.com>
2026-05-03 09:26:21 +1000

    feat: initialize travel journal project with React and Vite
    - Add package.json with dependencies and scripts for development
    - Create favicon.svg for the application
    - Add icons.svg containing various SVG icons for use in the app
    - Implement main App component and Header component for the layout
    - Add basic CSS styles for the header and overall layout
    - Set up entry point in main.jsx to render the App component
    - Configure Vite with a basic setup in vite.config.js

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <img src="../image/globe-svgrepo-com.svg" alt="globe-logo" className="globe-logo" />
+            <span>my travel journal</span>
+        </header>
+    )
+}
