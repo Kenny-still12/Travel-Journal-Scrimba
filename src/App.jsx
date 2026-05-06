23a78d2853564014739e3d233caee5b82d1edad5
Kenneth Sonoda <114664532+Kenny-still12@users.noreply.github.com>
2026-05-06 21:28:33 +1000

    refined app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,7 @@ export default function App() {
   const entries = data.map(entry => {
     return <Entry
       key={entry.id}
-      img={
-        {
-          src: entry.img.src,
-          alt: entry.img.alt
-        }}
-      country={entry.country}
-      link={entry.googleMapsLink}
-      title={entry.title}
-      dates={entry.dates}
-      text={entry.text}
+      entry={entry}
     />
   })
 
--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -2,18 +2,18 @@ export default function Entry(props) {
     return (
         <main>
             <div className="entry-image">
-                <img src={props.img.src} alt={props.img.alt} className="entry-image" />
+                <img src={props.entry.img.src} alt={props.entry.img.alt} className="entry-image" />
             </div>
             <div className="entry-description">
                 <div className="entry-location">
                     <img src="./image/location-pin.png" alt="" className="location-pin" />
-                    <span>{props.country}</span>
-                    <a href={props.link}>View on Google Maps</a>
+                    <span>{props.entry.country}</span>
+                    <a href={props.entry.link}>View on Google Maps</a>
                 </div>
-                <h1 className="entry-title">{props.title}</h1>
+                <h1 className="entry-title">{props.entry.title}</h1>
                 <div className="description">
-                    <p className="entry-dates">{props.dates}</p>
-                    <p className="text">{props.text}</p>
+                    <p className="entry-dates">{props.entry.dates}</p>
+                    <p className="text">{props.entry.text}</p>
                 </div>
             </div>
 
